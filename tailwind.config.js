4b6f5a7b3b8cc30ff9171ec2f2694e1d8612dd3d
nurullailia20 <nurullailia720@gmail.com>
2024-05-05 08:58:52 +0700

    [style] profilecard layouting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
+    colors: {
+      "red" : "#841818"
+    }
   },
   plugins: [],
 }
-
